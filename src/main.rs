f052791aa0e75978a837096e6746a579e4c69fc3
Dominik Ritz <dom_ritz@icloud.com>
2025-12-27 23:56:34 -0500

    Basic trunk setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .insert_resource(ClearColor(Color::srgb(0.08, 0.75, 0.72)))
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2d);
 }
